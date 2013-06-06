fd1fccc25a85f4cbbd98cf66a14ea03ce94856c7
Andy Gross <andy@andygross.org>
2013-06-06 09:38:08 -0700

    checkpoint old commits from the move-stanchion-to-cs effort

--- src/stanchion_sup.erl
+++ src/stanchion_sup.erl
@@ -48,40 +48,8 @@ start_link() ->
                          integer()},
                         [supervisor:child_spec()]}}.
 init([]) ->
-    case application:get_env(stanchion, stanchion_ip) of
-        {ok, Ip} ->
-            ok;
-        undefined ->
-            Ip = "0.0.0.0"
-    end,
-    case application:get_env(stanchion, stanchion_port) of
-        {ok, Port} ->
-            ok;
-        undefined ->
-            Port = 80
-    end,
-
-    %% Create child specifications
-    WebConfig1 = [
-                 {dispatch, stanchion_web:dispatch_table()},
-                 {ip, Ip},
-                 {port, Port},
-                 {nodelay, true},
-                 {log_dir, "log"},
-                 %% {rewrite_module, stanchion_wm_rewrite},
-                 {error_handler, stanchion_wm_error_handler}],
-    case application:get_env(stanchion, ssl) of
-        {ok, SSLOpts} ->
-            WebConfig = WebConfig1 ++ [{ssl, true},
-                                       {ssl_opts, SSLOpts}];
-        undefined ->
-            WebConfig = WebConfig1
-    end,
-    Web = {webmachine_mochiweb,
-           {webmachine_mochiweb, start, [WebConfig]},
-           permanent, 5000, worker, dynamic},
     ServerSup = {stanchion_server_sup,
                  {stanchion_server_sup, start_link, []},
                  permanent, 5000, worker, dynamic},
-    Processes = [ServerSup, Web],
+    Processes = [ServerSup],
     {ok, { {one_for_one, 10, 10}, Processes} }.
